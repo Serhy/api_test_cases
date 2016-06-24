a7b8af27a71b316e37c8dbac0f6e045969483a3e
Serhy <Serhy@Serhy-PC>
2016-06-24 08:41:37 +0300

    Pushing the last changes

---
+++ 111/Serhy_2.0/replacements.py
@@ -0,0 +1,16 @@
+'''
+Created on Jun 21, 2016
+
+@author: Serhy
+'''
+
+expected_ids = {
+                u'87-792': u'API-707',
+                u'87-793': u'API-708',
+                u'87-810': u'API-725'
+                }
+
+expected_user_details = ['root', 's1iderorama+codespace@gmail.com', 'root']
+error_for_invalid_user_login_name = "User not found or you have no permission to read user."
+
+list_of_projects = [[u'API test', u'API'], [u'JJ', u'JJ'], [u'VITALIY', u'VITALIY']]
